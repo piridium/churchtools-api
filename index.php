0e346d2c34037df8c5c219e0805f9240c7a71962
Patrick Itten <info@patrickitten.ch>
2022-03-17 16:09:29 +0100

    ct api working and getting data

--- index.php
+++ index.php
@@ -9,12 +9,13 @@ require __DIR__ . '/vendor/autoload.php';
 // https://github.com/vineyardkoeln/churchtools-api
 
 use \CTApi\CTConfig;
+use \CTApi\Requests\EventRequest;
+use \CTApi\Requests\EventAgendaRequest;
+include './credentials.inc';
 
 //set the url of your churchtools application api
 //important! ApiUrl must end with Top-Level-Domain. No paths allowed!
 CTConfig::setApiUrl("https://rebgarten.church.tools");
-
-include './credentials.inc';
 
 //authenticates the application and load the api-key into the config
 try {
@@ -27,8 +28,35 @@ try {
   return;
 }
 
-
 // if everything works fine, the api-key is stored in your config
 $apiKey = CTConfig::getApiKey();
 
-print_r($apiKey);
+// Retrieve all events from now
+$events = EventRequest::where('from', date('Y-m-d', strtotime('today')))
+  ->where('to', date('Y-m-d', strtotime('+2months')))
+  ->orderBy('startDate')
+  ->get();
+
+foreach ($events as $event){
+  $event = EventRequest::find($event->getId());
+  $eventServices = $event?->getEventServices() ?? [];
+  $speakers = [];
+  foreach ($eventServices as $eventService) {
+    if ($eventService?->requestService()?->getId() == 1) {
+      if ($eventService?->getName()){
+        $speakers[] = $eventService?->getName();
+      }
+    }
+  }
+
+  $title = "(" . $event?->getId() . ") ";
+  $title .= $event?->getName();
+  $title .= empty($speakers) ? '' : ' mit ' . join(', ', $speakers);
+  echo $title, "\n";
+  // echo $event->getDescription(), "\n";
+  echo $event?->getStartDate(), " - ", $event?->getEndDate(), "\n";
+  echo 'Kalender: ', $event?->getCalendar()->getTitle(), "\n";
+  // echo "uuid: ", $event->getGuid(), "\n";
+  echo "\n";
+  // $agenda = $event?->requestAgenda();
+}
